c9eb543ee08ef3575c3740065dcf92bc4e53228d
brunabaudel <brubaudel@gmail.com>
2014-10-23 13:29:26 -0300

    Classe ConvertToCMYK adicionada

---
+++ app/src/main/java/dm/ufpe/br/colorpicker/ConvertToCMYK.java
@@ -0,0 +1,74 @@
+package dm.ufpe.br.colorpicker;
+
+import android.content.Context;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by brunasantana on 23/10/14.
+ */
+public class ConvertToCMYK {
+
+    private static final int RANGE = 255;
+    private int red;
+    private int green;
+    private int blue;
+
+    public ConvertToCMYK () {
+        this.red = 0;
+        this.green = 0;
+        this.blue = 0;
+    }
+
+    public double get_red() {
+        return (double) this.red/RANGE;
+    }
+
+    public double get_green() {
+        return (double) this.green/RANGE;
+    }
+
+    public double get_blue() {
+        return (double) this.blue/RANGE;
+    }
+
+    public void set_red(int red) {
+        this.red = red;
+    }
+
+    public void set_green(int green) {
+        this.green = green;
+    }
+
+    public void set_blue(int blue) {
+        this.blue = blue;
+    }
+
+    public String getKey() {
+        double k = calcKey() * 100;
+        return new DecimalFormat("#.##").format(k);
+    }
+
+    public String getCyan() {
+        double c = (1 - get_red() - calcKey()) / (1 - calcKey()) * 100;
+        return new DecimalFormat("#.##").format(c);
+    }
+
+    public String getMagenta() {
+        double m = (1 - get_green() - calcKey()) / (1 - calcKey()) * 100;
+        return new DecimalFormat("#.##").format(m);
+    }
+
+    public String getYellow() {
+        double y = (1 - get_blue() - calcKey()) / (1 - calcKey()) * 100;
+        return new DecimalFormat("#.##").format(y);
+    }
+
+    private double maxRGB(){
+        return Math.max(Math.max(get_blue(), get_green()), get_red());
+    }
+
+    private double calcKey() {
+        return 1 - maxRGB();
+    }
+}
--- app/src/main/java/dm/ufpe/br/colorpicker/MainActivity.java
+++ app/src/main/java/dm/ufpe/br/colorpicker/MainActivity.java
@@ -1,48 +1,36 @@
 package dm.ufpe.br.colorpicker;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.view.SurfaceView;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.File;
-import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class MainActivity extends Activity implements View.OnTouchListener, View.OnClickListener {
 
-    private static final int RANGE = 255;
-    private int red;
-    private int green;
-    private int blue;
+    private ConvertToCMYK convertToCMYK;
 
     private TextView txt_cyan;
     private TextView txt_magenta;
@@ -55,6 +43,7 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
 
     private FrameLayout frame_color1;
     private LinearLayout color_icon;
+    private RelativeLayout relative;
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private Uri fileUri;
@@ -71,9 +60,7 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
 
     private void initialize() {
 
-        this.red = 0;
-        this.green = 0;
-        this.blue = 0;
+        this.convertToCMYK = new ConvertToCMYK();
 
         this.txt_cyan = (TextView) findViewById(R.id.txt_cyan);
         this.txt_magenta = (TextView) findViewById(R.id.txt_magenta);
@@ -86,6 +73,7 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
 
         this.frame_color1 = (FrameLayout) findViewById(R.id.frame_color1);
         this.color_icon = (LinearLayout) findViewById(R.id.color_icon);
+        this.relative = (RelativeLayout) findViewById(R.id.relative);
 
         this.image.setImageResource(R.drawable.flor);
 
@@ -120,39 +108,56 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
         int x = (int)motionEvent.getX();
         int y = (int)motionEvent.getY();
 
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+        int pixel = 0;
+
         this.bitmap = Bitmap.createBitmap(image.getWidth(), image.getHeight(), Bitmap.Config.RGB_565);
         Canvas canvas = new Canvas(this.bitmap);
         image.draw(canvas);
 
        if((x >= 0 && x < this.bitmap.getWidth() && y >= 0 && y < this.bitmap.getHeight())) {
 
-           int pixel = this.bitmap.getPixel(x, y);
+           pixel = this.bitmap.getPixel(x, y);
 
-           this.red = Color.red(pixel);
-           this.green = Color.green(pixel);
-           this.blue = Color.blue(pixel);
+           red = Color.red(pixel);
+           green = Color.green(pixel);
+           blue = Color.blue(pixel);
 
-           this.txt_cyan.setText("Cyan: " + this.getCyan()+"");
-           this.txt_magenta.setText("Magenta: " + this.getMagenta()+"");
-           this.txt_yellow.setText("Yellow: " + this.getYellow()+"");
-           this.txt_key.setText("Key: " + this.getKey()+"");
+           this.convertToCMYK.set_red(red);
+           this.convertToCMYK.set_green(green);
+           this.convertToCMYK.set_blue(blue);
+
+           this.txt_cyan.setText("Cyan: " + this.convertToCMYK.getCyan()+"");
+           this.txt_magenta.setText("Magenta: " + this.convertToCMYK.getMagenta()+"");
+           this.txt_yellow.setText("Yellow: " + this.convertToCMYK.getYellow()+"");
+           this.txt_key.setText("Key: " + this.convertToCMYK.getKey()+"");
 
           // Log.i("COLOR", "Pixel: " + pixel);
            //Log.i("COLOR", "R: " + this.red + " G: " + this.green + " B: " + this.blue);
            Log.i("POSITION", "x: " + x + " y: " + y);
 
-           this.frame_color.setBackgroundColor(Color.rgb(this.red, this.green, this.blue));
+           this.frame_color.setBackgroundColor(Color.rgb(red, green, blue));
 
            this.frame_color1.setX(x + this.image.getX() - 75);
            this.frame_color1.setY(y-this.image.getY() - 100);
 
-          // this.frame_color1.setBackgroundColor(Color.rgb(this.red, this.green, this.blue));
-           this.color_icon.setBackgroundColor(Color.rgb(this.red, this.green, this.blue));
+           this.color_icon.setBackgroundColor(Color.rgb(red, green, blue));
 /*
            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
                this.frame_color1.setVisibility(View.INVISIBLE);
            } else {
                this.frame_color1.setVisibility(View.VISIBLE);
+           }
+
+
+           if(this.relative.getHeight()-this.image.getHeight() <= x + this.image.getX() - 75+this.frame_color1.getHeight()) {
+               Animation rotateAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_layout);
+               LayoutAnimationController animController = new LayoutAnimationController(rotateAnim, 0);
+               frame_color1.setLayoutAnimation(animController);
+              // color_icon.setY(y+this.image.getY() - 100);
+
            }
 */
            this.frame_color1.setVisibility(View.VISIBLE);
@@ -160,53 +165,6 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
 
         bitmap.recycle();
         return false;
-    }
-
-
-    /**
-     *
-     * Convert RGB to CMYK
-     *
-     */
-
-    private double get_red() {
-        return (double) this.red/RANGE;
-    }
-
-    private double get_green() {
-        return (double) this.green/RANGE;
-    }
-
-    private double get_blue() {
-        return (double) this.blue/RANGE;
-    }
-
-    private double calcKey() {
-        return 1 - maxRGB();
-    }
-
-    private String getKey() {
-        double k = calcKey() * 100;
-        return new DecimalFormat("#.##").format(k);
-    }
-
-    private String getCyan() {
-        double c = (1 - get_red() - calcKey()) / (1 - calcKey()) * 100;
-        return new DecimalFormat("#.##").format(c);
-    }
-
-    private String getMagenta() {
-        double m = (1 - get_green() - calcKey()) / (1 - calcKey()) * 100;
-        return new DecimalFormat("#.##").format(m);
-    }
-
-    private String getYellow() {
-        double y = (1 - get_blue() - calcKey()) / (1 - calcKey()) * 100;
-        return new DecimalFormat("#.##").format(y);
-    }
-
-    private double maxRGB(){
-        return Math.max(Math.max(get_blue(), get_green()), get_red());
     }
 
     /**
